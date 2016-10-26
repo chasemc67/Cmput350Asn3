b8d82b1e882d583d2a92e0363506b21c7d972279
chasemc67 <chasemc67@gmail.com>
2016-10-25 18:26:48 -0600

    Testing

--- a3/Test.C
+++ a3/Test.C
@@ -6,7 +6,9 @@ using namespace std;
 int main() {
 	Grid * grid = new Grid(500, 250);
 
+	cout << "Width:";
 	cout << grid->getWidth() << endl;
+	cout << "Height:";
 	cout << grid->getHeight() << endl;
 
 	delete grid;
