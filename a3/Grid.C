b40db454b45b5755b4d0b1e8eb4c8dae66bf09fa
chasemc67 <chasemc67@gmail.com>
2016-10-30 19:36:01 -0600

    Testing

--- a3/Grid.C
+++ a3/Grid.C
@@ -80,7 +80,7 @@ int Grid::findShortestPath(int size, int x1, int y1, int x2, int y2,
 	Node * endNode = new Node(x2, y2);
 
 	Node * startNode = new Node(x1, y1);
-	startNode -> cameFrom = startNode;
+	//startNode -> cameFrom = startNode;
 	startNode -> gScore = 0;
 	startNode -> fScore = getHeuristic(&startNode, &endNode);
 
